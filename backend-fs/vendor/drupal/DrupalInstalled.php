b32e29aa7c3cd97c5862e9dc9722e78312d6bf12
bthvedt <bthvedt@gmail.com>
2026-06-29 16:49:02 -0500

    Wired up the oauth module with password grant

--- backend-fs/vendor/drupal/DrupalInstalled.php
+++ backend-fs/vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '822c83bbf8a68477';
+  public const string VERSIONS_HASH = '4c91e5661e78d217';
 
 }
